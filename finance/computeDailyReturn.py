450a0ac526f0a6140bad0f25ae2814d0baf8e6d0
pongem <Pong@Pongsaks-MacBook-Pro.local>
2017-03-04 21:46:40 +0700

    compute daily return

--- finance/computeDailyReturn.py
+++ finance/computeDailyReturn.py
@@ -6,21 +6,22 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-	start_date = "2016-12-01"
+	start_date = "2016-10-01"
 	end_date = "2017-03-01"
 	arrSymbols = ["CPF","PTT", "CPALL","TTW"]
 	helper = Stock_Helper()
 	df = helper.buildClosePrice(start_date, end_date, arrSymbols)
 
 	# slice the array
-	newdf = df.ix["2017-01-01" : "2017-02-21", ['CPF'] ]
+	newdf = df.ix["2016-10-01" : "2017-03-01", ['CPF'] ]
 
-	# rolling mean
-	#ax = newdf.plot(label='newdf')
-	# rm = pd.rolling_mean(newdf['CPF'], window=5)
-	# rm.plot( label='rolling mean', ax=ax)
-	# plt.show()
+	# compute daily return
+	dailyReturn = helper.compute_daily_returns(newdf)\
+	#dailyReturn.plot()
+	#plt.show()
+
+	# compute histogram
+	dailyReturn.hist( bins=20 )
+	plt.show()
 
 
-	# calculate mean
-	# print (newdf.mean())
--- finance/mod_Stock_Analyze_Helper.py
+++ finance/mod_Stock_Analyze_Helper.py
@@ -31,3 +31,10 @@ class Stock_Helper(object):
 
 		return df1
 		
+	def compute_daily_returns(self, df):
+		daily_returns = df.copy()
+		daily_returns[1:] = (df[1:]/df[:-1].values) - 1
+		daily_returns.ix[0, :] = 0
+		return daily_returns
+
+		
